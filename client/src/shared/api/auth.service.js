6658719a6ffa7eee03254233bc9be5bccf809849
ZiF1R <do-alex03@mail.ru>
2022-07-01 16:25:59 +0300

    Add shared folder

---
+++ client/src/shared/api/auth.service.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { baseUsersUrl as baseUrl } from "../constants/routes";
+
+axios.defaults.headers.common = {
+  "Content-Type": "application/json",
+};
+
+const createUser = async (data) => {
+  const request = axios.post(baseUrl, data);
+  return await request;
+};
+
+const login = async (email, password) => {
+  const request = axios.get(baseUrl + `?email=${email}&password=${password}`);
+  return await request;
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  login,
+  createUser,
+};
---
+++ client/src/shared/constants/routes.js
@@ -0,0 +1,3 @@
+module.exports = {
+  baseUsersUrl: "/api/users",
+}
